2914d65da169d577360959288529516b38ef77d7
ashusatyarthi-wq <ashusatyarthi@gmail.com>
2026-06-26 22:35:53 +0530

    Fix WebGL crash in production

--- src/components/WebGlBackground.jsx
+++ src/components/WebGlBackground.jsx
@@ -15,7 +15,7 @@ function LiquidMetalShape() {
   return (
     <Float speed={2} rotationIntensity={0.5} floatIntensity={1}>
       <mesh ref={meshRef} scale={1.8}>
-        <sphereGeometry args={[1, 128, 128]} />
+        <sphereGeometry args={[1, 64, 64]} />
         <MeshDistortMaterial
           color="#a3a3a3"
           envMapIntensity={1}
@@ -34,14 +34,12 @@ function LiquidMetalShape() {
 export default function WebGlBackground() {
   return (
     <div className="fixed inset-0 z-0 bg-[#050505]">
-      <Canvas camera={{ position: [0, 0, 4], fov: 45 }} dpr={[1, 2]}>
-        <ambientLight intensity={0.2} />
-        <directionalLight position={[10, 10, 5]} intensity={1} color="#ffffff" />
-        <directionalLight position={[-10, -10, -5]} intensity={0.5} color="#d4d4d4" />
+      <Canvas camera={{ position: [0, 0, 4], fov: 45 }} dpr={[1, 1.5]}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1.5} color="#ffffff" />
+        <directionalLight position={[-10, -10, -5]} intensity={1} color="#d4d4d4" />
+        <pointLight position={[0, 5, 0]} intensity={1} color="#ffffff" />
         <LiquidMetalShape />
-        <Suspense fallback={null}>
-          <Environment preset="city" />
-        </Suspense>
       </Canvas>
     </div>
   );
