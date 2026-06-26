6aad30ea7c1601a9582fc6f944fe93918e8ff5ed
ashusatyarthi-wq <ashusatyarthi@gmail.com>
2026-06-26 22:58:23 +0530

    Revert synthetic Lightformers to fix ugly lighting, restore beautiful city preset

--- src/components/WebGlBackground.jsx
+++ src/components/WebGlBackground.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { MeshDistortMaterial, Environment, Float, Lightformer } from '@react-three/drei';
+import { MeshDistortMaterial, Environment, Float } from '@react-three/drei';
 
 function LiquidMetalShape() {
   const meshRef = useRef();
@@ -18,7 +18,7 @@ function LiquidMetalShape() {
         <sphereGeometry args={[1, 128, 128]} />
         <MeshDistortMaterial
           color="#a3a3a3"
-          envMapIntensity={2}
+          envMapIntensity={1}
           clearcoat={1}
           clearcoatRoughness={0}
           metalness={1}
@@ -41,16 +41,9 @@ export default function WebGlBackground() {
         
         <LiquidMetalShape />
         
-        <Environment resolution={256}>
-          {/* Ceiling light */}
-          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
-          {/* Side lights */}
-          <Lightformer intensity={2} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[20, 0.1, 1]} />
-          <Lightformer rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[20, 0.5, 1]} />
-          <Lightformer rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[20, 1, 1]} />
-          {/* Soft background light */}
-          <Lightformer form="ring" color="#4c1d95" intensity={2} scale={10} position={[-15, 4, -18]} target={[0, 0, 0]} />
-        </Environment>
+        <Suspense fallback={null}>
+          <Environment preset="city" />
+        </Suspense>
       </Canvas>
     </div>
   );
